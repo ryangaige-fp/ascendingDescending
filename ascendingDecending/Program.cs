33cae390fd04c3c9bd5e12fc99c22aa6d2f5c442
Ryan Gaige <Ryan@fencepencil.com>
2018-09-26 18:37:32 -0600

    Initial commit

---
+++ ascendingDecending/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ascendingDecending
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] arr = {};
+            int[] arr2 = {};
+
+            Console.Write("Enter 4 numbers: ");
+
+            Console.ReadLine();
+
+            Array.Sort(arr);
+            Array.Reverse(arr2);
+
+            for (int i = 0; i < max; i++)
+            {
+
+            }
+
+            {
+
+                Console.WriteLine(arr);
+            }
+
+
+
+
+
+
+
+            }
+
+
+        }
+    }
+
